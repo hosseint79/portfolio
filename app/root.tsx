324e26bbbdaa3ca5b1f3499c7f9974016841029e
hossein.w7979 <hossein.w7979@gmail.com>
2022-05-31 15:36:19 +0430

    install tailwind and init

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import "./styles/__global.styles";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,38 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <div className="mx-auto mt-16 max-w-7xl text-center">
-        <Link to="/posts" className="text-xl text-blue-600 underline">
-          Blog Posts
-        </Link>
-      </div>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            22
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <button className="bg-cyan-500 shadow-lg shadow-cyan-500/50">
+        Subscribe
+      </button>
     </div>
   );
 }
