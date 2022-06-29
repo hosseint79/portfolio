feed09fc35f3e1eb5e7ffd8cd32f4cc71bae5d9e
Hossein <hossein.w7979@gmail.com>
2022-06-29 20:00:46 +0430

    style: complete contact-me info-items

--- app/components/ContactMe/ContactMe.tsx
+++ app/components/ContactMe/ContactMe.tsx
@@ -1,4 +1,6 @@
+import { AiOutlineMail } from "react-icons/ai";
 import { FiPhone } from "react-icons/fi";
+import { TiLocation } from "react-icons/ti";
 import { Container } from "../common/Container/Container";
 import { SectionHeader } from "../common/SectionHeader/SectionHeader";
 
@@ -7,11 +9,11 @@ function ContactMe() {
     return (  
         <Container>
             <SectionHeader title="Contact Me" caption="Get in touch"/>
-            <div className=" grid gap-6 py-8 grid-cols-5">
+            <div className=" grid gap-6 py-8 grid-cols-5 px-16" >
                  <div className="col-span-2 ">
-                    <div className="flex items-center text-white">
+                    <div className="flex items-center text-white mb-6">
                         
-                        <FiPhone size={30} className="mx-4 text-indigo-800"  />
+                        <FiPhone size={30} className="mx-4 text-indigo-600"  />
                         
                         <div>
                             <h5 className="font-bold text-lg mb-[2px]">
@@ -19,6 +21,32 @@ function ContactMe() {
                             </h5>
                             <h6>
                                 09397233907
+                            </h6>
+                        </div>
+                    </div>
+                      <div className="flex items-center text-white mb-6">
+                        
+                        <AiOutlineMail size={30} className="mx-4 text-indigo-600"  />
+                        
+                        <div>
+                            <h5 className="font-bold text-lg mb-[2px]">
+                               Email
+                            </h5>
+                            <h6>
+                                hossein.w7979@gmail.com
+                            </h6>
+                        </div>
+                    </div>
+                      <div className="flex items-center text-white mb-6">
+                        
+                        <TiLocation size={33} className="mx-4 text-indigo-600"  />
+                        
+                        <div>
+                            <h5 className="font-bold text-lg mb-[2px]">
+                               Location
+                            </h5>
+                            <h6>
+                                iran - sari
                             </h6>
                         </div>
                     </div>
