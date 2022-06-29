adceea91875f0b5c51ffa8e80b0ddf47b125005a
Hossein <hossein.w7979@gmail.com>
2022-06-29 19:21:57 +0430

    feat: add info-item component in contact-me section

--- app/components/ContactMe/ContactMe.tsx
+++ app/components/ContactMe/ContactMe.tsx
@@ -1,8 +1,32 @@
+import { FiPhone } from "react-icons/fi";
+import { Container } from "../common/Container/Container";
+
+
 function ContactMe() {
     return (  
-        <div className="bg-slate-200 py-8">
-            test
-        </div>
+        <Container>
+            <div className=" grid gap-6 py-8 grid-cols-5">
+                 <div className="col-span-2 ">
+                    <div className="flex items-center text-white">
+                        
+                        <FiPhone size={28} className="mx-4"/>
+                        
+                        <div>
+                            <h5 className="font-bold text-lg mb-[2px]">
+                               Call Me
+                            </h5>
+                            <h6>
+                                09397233907
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-3">
+                    ads
+                </div>
+            </div>
+        </Container>
+
     );
 }
 
