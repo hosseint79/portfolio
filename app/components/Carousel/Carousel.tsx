81f8aeead86c9ccd9cb0248c07bd1fe212225538
Hossein <hossein.w7979@gmail.com>
2022-07-07 12:59:37 +0430

    style: change position of 3d-carousel next and before button

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -2,19 +2,21 @@ import React, { Component } from "react";
 import Carousel from "react-spring-3d-carousel";
 
 import { config } from "react-spring";
+import { Container } from "../common/Container/Container";
+import { SectionHeader } from "../common/SectionHeader/SectionHeader";
 
 let slides = [
   {
     key: 11,
-    content: <img src="https://picsum.photos/800/801/?random" alt="1" />
+    content: <img src="https://picsum.photos/800/801/?random" alt="1" style={{width:"500px",height:"500px"}} />
   },
   {
     key: 22,
-    content: <img src="https://picsum.photos/800/802/?random" alt="2" />
+    content: <img src="https://picsum.photos/800/802/?random" alt="2" style={{width:"500px",height:"500px"}}/>
   },
   {
     key: 33,
-    content: <img src="https://picsum.photos/600/803/?random" alt="3" />
+    content: <img src="https://picsum.photos/600/803/?random" alt="3" style={{width:"500px",height:"500px"}}/>
   },
 
 
@@ -32,17 +34,23 @@ export default class Example extends Component {
 
   render() {
     return (
-      <div style={{ width: "800px", height: "500px", margin: "0 auto" }}>
-        <Carousel
-          slides={slides}
-          goToSlide={this.state.goToSlide}
-          offsetRadius={this.state.offsetRadius}
-          showNavigation={this.state.showNavigation}
-          animationConfig={this.state.config}
-        />
-      
-         
-      </div>
+        
+        <Container>
+            <SectionHeader title="Projects" caption="Some of my projects"/>
+            <div className="px-16 threeD-carousel-button" style={{ height: "400px", margin: "0 auto" }} >
+                <Carousel
+                    slides={slides}
+                    goToSlide={this.state.goToSlide}
+                    offsetRadius={this.state.offsetRadius}
+                    showNavigation={this.state.showNavigation}
+                    animationConfig={this.state.config}
+
+                />
+            
+                
+            </div>
+        </Container>
+
     );
   }
 }
