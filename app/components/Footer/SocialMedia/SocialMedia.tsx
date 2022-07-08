d58f19a5cde19e59fdbd5f188cb814409c863a9b
Hossein <hossein.w7979@gmail.com>
2022-07-08 18:46:28 +0430

    feat: add simple animation

--- app/components/Footer/SocialMedia/SocialMedia.tsx
+++ app/components/Footer/SocialMedia/SocialMedia.tsx
@@ -1,31 +1,57 @@
 
 
-
-
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsLinkedin, BsWhatsapp } from "react-icons/bs";
 import { TiSocialGithub } from "react-icons/ti";
+import { useEffect } from "react";
+
+
+interface IProps{
+duration:number;
+href:string;
+Icon:any;
+hoverBg:string
+}
+
+function SocialItem({duration,href,Icon,hoverBg}:IProps) {
+
+    const squareVariants = {
+        visible: { opacity: 1,y:0, transition: { duration: duration } },
+        hidden: { opacity: 0,y:20 }
+    };
+
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={squareVariants}
+     className={"p-1 w-16 h-16 flex items-center justify-center mx-1 bg-[#0b172f87] duration-500 rounded-full " + hoverBg }
+    >
+         <a href={href} target="__blank"> <Icon /> </a>
+    </motion.div>
+  );
+}
+
 
 
 function SocialMedia() {
     return (  
         <div className="flex my-14">
-            <div className="p-1 w-16 h-16 flex items-center justify-center mx-1 bg-[#0b172f87] duration-500 rounded-full hover:bg-slate-600">
-                <a href="https://github.com/hosseint79" target="__blank"><TiSocialGithub size={50}/></a>
-               
-            </div>
-            <div className="p-1 w-16 h-16 flex items-center justify-center mx-1 bg-[#0b172f87] duration-500 rounded-full hover:bg-green-700">
-       
-               <a href="https://api.whatsapp.com/send?phone=09397233907" target="__blank"><BsWhatsapp size={30}/></a>
-                
-            </div>
-            <div className="p-1 w-16 h-16 flex items-center justify-center mx-1 bg-[#0b172f87] duration-500 rounded-full hover:bg-blue-600">
-                <BsLinkedin size={27}/>
-            </div>
-            <div className="p-1 w-16 h-16 flex items-center justify-center mx-1 bg-[#0b172f87]  duration-500 rounded-full hover:bg-red-700">
-                
-                <a href="mailto:hossein.w7979@gmail.com" target="__blank"><AiOutlineMail size={27}/></a>
-            </div>
+            <SocialItem duration={0.7} hoverBg="hover:bg-slate-600" Icon={() => <TiSocialGithub size={50}/>} href="https://github.com/hosseint79"/>
+            <SocialItem duration={1} hoverBg="hover:bg-green-700" Icon={() => <BsWhatsapp size={30}/>} href="https://api.whatsapp.com/send?phone=09397233907" />
+            <SocialItem duration={1.3} hoverBg="hover:bg-blue-600" Icon={() => <BsLinkedin size={27}/>} href=""/>
+            <SocialItem duration={1.6} hoverBg="hover:bg-red-700" Icon={() => <AiOutlineMail size={27}/>} href="mailto:hossein.w7979@gmail.com"/>
         </div>
     );
 }
--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation = () => {
   return (
     <div className=" md:h-full md:w-auto w-full z-50 fixed bottom-0 md:top-0  md:left-8  flex items-center">
       <div
-        className="w-screen  md:mx-6 flex-row h-10 md:w-10 justify-between bg-zinc-800 opacity-60 z-50  p-4 box-content  md:rounded-[35px] flex items-center md:flex-col "
+        className="w-screen  md:mx-6 flex-row h-10 md:h-auto md:w-10 justify-between bg-zinc-800 opacity-60 z-50  p-4 box-content  md:rounded-[35px] flex items-center md:flex-col "
       >
         <NavigationItem Icon={AiOutlineHome} />
         <NavigationItem Icon={AiOutlineUser} />
