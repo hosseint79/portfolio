de7ccd10ebbe32a64adaa21b04eb4d7f89be475b
Hossein <hossein.w7979@gmail.com>
2022-07-19 18:17:18 +0430

    style: fix reponsive of short-info-item

--- app/components/Skills/ShortInfo/ShortInfoItem/ShortInfoItem.tsx
+++ app/components/Skills/ShortInfo/ShortInfoItem/ShortInfoItem.tsx
@@ -5,12 +5,12 @@ interface IProps {
 }
 function ShortInfoItem({count,firstTitle,secondTitle}:IProps) {
     return ( 
-            <div className="text-center mx-5 md:mx-16">
-                <h5 className="font-semibold text-2xl">{count}+</h5>
-                <h6 className="text-[#BBB9C2]">
+            <div className="text-center mx-3 sm:mx-5 md:mx-16">
+                <h5 className="font-semibold text-base md:text-2xl">{count}+</h5>
+                <h6 className="text-[#BBB9C2] text-sm md:text-xl">
                     {firstTitle}
                 </h6>
-                <h6 className="text-[#BBB9C2]">
+                <h6 className="text-[#BBB9C2] text-sm md:text-lg">
                     {secondTitle}
                 </h6>
             </div> 
