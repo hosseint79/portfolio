1407ae8c04fe972d33b42dc1fe518b02513df226
Hossein <hossein.w7979@gmail.com>
2022-07-26 16:09:30 +0430

    chore: add prettier to project

---
+++ app/routes/test.tsx
@@ -0,0 +1,11 @@
+const Test = () => {
+  return (
+    <div className="flex flex-no-wrap bg-gray-200 w-5/6">
+      <div className="w-2/5 flex-shrink-0 bg-teal-400 p-4 m-2">1</div>
+      <div className="w-2/5 flex-shrink-0 bg-teal-400 p-4 m-2">2</div>
+      <div className="w-2/5 flex-shrink-0 bg-teal-400 p-4 m-2">3</div>
+    </div>
+  );
+};
+
+export default Test;
