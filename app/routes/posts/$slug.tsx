6e98ffe2d7b5b5fd0239d360146b50b215812ffe
Hossein <hossein.w7979@gmail.com>
2022-08-04 22:17:08 +0430

    style: responsive blog page

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -4,6 +4,7 @@ import { json } from '@remix-run/node'
 
 import { BlogLayout } from '~/components/Layout/BlogLayout'
 import { GetBlog } from '~/lib/utilities/test.server'
+import { Container } from '~/components/common/Container/Container'
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { html, frontmatter } = GetBlog(params.slug + '')
@@ -20,31 +21,36 @@ export default function PostSlug() {
   console.log('html', html)
   return (
     <BlogLayout>
-      <main className="min-h-screen bg-[#110f1c]" style={{ direction: 'rtl' }}>
-        <article className="container mx-auto py-14 px-8 text-[#b4b8cb]">
-          <div>
-            <img
-              src={frontmatter?.cover_image}
-              alt=""
-              className="mt-12 mb-16 inline-block h-auto w-full rounded-lg"
-            />
-            <h1 className="mb-2 text-xl font-bold text-white">
-              {frontmatter?.title}
-            </h1>
-            <div className="mb-2 text-sm font-bold text-[#A9ADC1] ">
-              {' '}
-              تاریخ : {frontmatter?.date}{' '}
+      <Container>
+        <main
+          className="min-h-screen bg-[#110f1c]"
+          style={{ direction: 'rtl' }}
+        >
+          <article className=" py-14 text-[#b4b8cb]">
+            <div>
+              <img
+                src={frontmatter?.cover_image}
+                alt=""
+                className="mt-12 mb-16 inline-block h-auto w-full rounded-lg"
+              />
+              <h1 className="mb-2 text-xl font-bold text-white">
+                {frontmatter?.title}
+              </h1>
+              <div className="mb-2 text-sm font-bold text-[#A9ADC1] ">
+                {' '}
+                تاریخ : {frontmatter?.date}{' '}
+              </div>
+              <div className="post-body">
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: html,
+                  }}
+                ></div>
+              </div>
             </div>
-            <div className="post-body">
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: html,
-                }}
-              ></div>
-            </div>
-          </div>
-        </article>
-      </main>
+          </article>
+        </main>
+      </Container>
     </BlogLayout>
   )
 }
