7c24cb64a09f957910f80481eea7ac407bb128e1
Hossein <hossein.w7979@gmail.com>
2022-08-13 11:17:48 +0430

    style: change border and icon color of navigation items

--- app/components/Layout/Layout/Navigation/Navigation-Item/Navigation-Item.tsx
+++ app/components/Layout/Layout/Navigation/Navigation-Item/Navigation-Item.tsx
@@ -20,7 +20,7 @@ const NavigationItem: FC<PropsType> = ({ Icon, go, index }) => {
     <Link to={'#' + go}>
       <Icon
         className={`opacity-8 box-border rounded-full border-2 border-neutral-900 bg-neutral-900 p-2 text-white hover:border-indigo-900 hover:text-indigo-900 md:mb-4 
-          ${index === trueIndex() ? 'border-indigo-800 text-indigo-800 ' : ''}
+          ${index === trueIndex() ? 'border-[#0e82a7] text-[#0e82a7]' : ''}
         `}
         size={45}
       />
