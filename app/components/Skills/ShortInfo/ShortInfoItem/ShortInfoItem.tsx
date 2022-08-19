f71d3af2ae11d440e64dfab1bb4c075650085b39
Sobhan <sobhanyazdanjo@gmail.com>
2022-08-19 14:33:22 +0430

    fix: Remove additional plus in years

--- app/components/Skills/ShortInfo/ShortInfoItem/ShortInfoItem.tsx
+++ app/components/Skills/ShortInfo/ShortInfoItem/ShortInfoItem.tsx
@@ -6,7 +6,7 @@ interface IProps {
 function ShortInfoItem({ count, firstTitle, secondTitle }: IProps) {
   return (
     <div className="mx-3 mb-5 text-center sm:mx-5 lg:mx-16">
-      <h5 className="text-lg font-semibold lg:text-xl">{count}+</h5>
+      <h5 className="text-lg font-semibold lg:text-xl">{count}</h5>
       <h6 className="text-sm text-[#BBB9C2] ">{firstTitle}</h6>
       <h6 className="text-sm text-[#BBB9C2] ">{secondTitle}</h6>
     </div>
